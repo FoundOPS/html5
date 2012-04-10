1810dcaa942f45c43302939e38af898722b8dce3
apohl1111 <apohl@foundops.com>
2012-04-09 21:34:59 -0400

    Added map. Renamed project to foundOPS

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {template: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+angular.module('foundOPS', ['foundOPS.filters', 'foundOPS.services', 'foundOPS.directives']).
+  config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.when('/routesmap', { template: 'partials/routesmap.html', controller: RoutesMapCtrl });
+      $routeProvider.when('/view2', { template: 'partials/partial2.html', controller: MyCtrl2 });
+      $routeProvider.otherwise({ redirectTo: '/routesmap' });
   }]);
