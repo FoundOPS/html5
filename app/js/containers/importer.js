356b4ed2d43174720ec120af02254f938ae38815
apohl1111 <apohl@foundops.com>
2012-06-19 20:30:35 -0400

    continue with importer

--- app/js/containers/importer.js
+++ app/js/containers/importer.js
@@ -25,12 +25,144 @@ require.config({
     }
 });
 
+//"lib/swfupload",
 require(["jquery", "lib/kendo.all.min", "lib/csv"], function ($, m, csv) {
     var importer = {};
+    var grid = {};
     var app;
 
     //Start the application
     app = new kendo.mobile.Application($(document.body), {initial: "views/importerUpload.html"});
 
-    var parsedCsv = csv.parse();
+//region Importer
+    importer.initialize = function () {
+//        var settings = {
+//            upload_url: "http://www.swfupload.org/upload.php",
+//            flash_url: "lib/swfupload.swf",
+//
+//            //Button settings
+//            button_placeholder_id: "spanButtonPlaceHolder",
+//            button_image_url: "img/upload.png",
+//            button_width: "61",
+//            button_height: "22",
+//            button_text_left_padding: 12,
+//            button_text_top_padding: 3,
+//
+//            file_size_limit: "2 MB",
+//            file_types: "*.csv",
+//            file_types_description: "CSV Files",
+//            file_upload_limit: 1,
+//            file_queue_limit: 1,
+//            custom_settings: {
+//                progressTarget: "fsUploadProgress",
+//                cancelButtonId: "btnCancel"
+//            },
+//            debug: true,
+//
+//
+//            // The event handler functions are defined in handlers.js
+//            file_queued_handler: fileQueued,
+//            file_queue_error_handler: fileQueueError,
+//            file_dialog_complete_handler: fileDialogComplete,
+//            upload_start_handler: uploadStart,
+//            upload_progress_handler: uploadProgress,
+//            upload_error_handler: uploadError,
+//            upload_success_handler: uploadSuccess,
+//            upload_complete_handler: uploadComplete,
+//            queue_complete_handler: queueComplete    // Queue plugin event
+//        };
+//
+//        var swfu = new SWFUpload(settings);
+
+        //create DropDownList from input HTML element
+        $("#serviceType").kendoDropDownList({
+            dataTextField: "Name",
+            dataValueField: "Id",
+            dataSource: {
+                type: "odata",
+                serverFiltering: false,
+                filter: [{
+                    field: "Name",
+                    operator: "contains",
+                    value: "Star Wars"
+                },{
+                    field: "BoxArt.SmallUrl",
+                    operator: "neq",
+                    value: null
+                }],
+                transport: {
+                    read: "http://odata.netflix.com/Catalog/Titles"
+                }
+            },
+            change: onChange
+        });
+
+        //get a reference to the DropDownList
+        var color = $("#serviceType").data("kendoDropDownList");
+        color.select(0);
+
+        function onChange() {
+        };
+
+        importer.upload = function () {
+            app.navigate("views/importerGrid.html");
+        };
+    };
+
+    window.importer = importer;
+
+    //var parsedCsv = csv.parse();
+//endregion
+
+//region Grid
+    grid.initialize = function () {
+        var data = [{header: "Client Name", value: "Meyer", name: ""},
+            {header: "Phone", value: "123-456-7890", name: ""}]
+
+        $("#fields").kendoGrid({
+            dataSource: data,
+            schema: {
+                model: {
+                    id: "header",
+                    fields: {
+                        header: { editable: false},
+                        value: { editable: false},
+                        name: { editable: true}
+                    }
+                }
+            },
+            columns: [
+                {
+                    field: "header",
+                    title: "Row 1"
+                },
+                {
+                    field: "value",
+                    title: "Row 2"
+                },
+                {
+                    field: "name",
+                    title: " ",
+                    editor: categoryDropDownEditor
+                }],
+            scrollable: true
+        });
+    }
+
+    function categoryDropDownEditor(container, options) {
+        $('<input data-text-field="CategoryName" data-value-field="CategoryName" data-bind="value:' + options.field + '"/>')
+            .appendTo(container)
+            .kendoDropDownList({
+                autoBind: false,
+                dataSource: {
+                    type: "odata",
+                    transport: {
+                        read: "http://demos.kendoui.com/service/Northwind.svc/Categories"
+                    }
+                }
+            });
+    }
+
+    window.grid = grid;
+    //endregion
 });
--- app/lib/swfupload.js
+++ app/lib/swfupload.js
@@ -11,7 +11,6 @@
  *
  */
 
-
 /* ******************* */
 /* Constructor & Init  */
 /* ******************* */
@@ -977,6 +976,309 @@ SWFUpload.Console.writeLine = function (message) {
 	} catch (ex) {
 		alert("Exception: " + ex.name + " Message: " + ex.message);
 	}
+};
+
+/*
+Queue Plug-in
+
+Features:
+*Adds a cancelQueue() method for cancelling the entire queue.
+    *All queued files are uploaded when startUpload() is called.
+    *If false is returned from uploadComplete then the queue upload is stopped.
+    If false is not returned (strict comparison) then the queue upload is continued.
+    *Adds a QueueComplete event that is fired when all the queued files have finished uploading.
+    Set the event handler with the queue_complete_handler setting.
+
+    */
+
+var SWFUpload;
+if (typeof(SWFUpload) === "function") {
+    SWFUpload.queue = {};
+
+    SWFUpload.prototype.initSettings = (function (oldInitSettings) {
+        return function () {
+            if (typeof(oldInitSettings) === "function") {
+                oldInitSettings.call(this);
+            }
+
+            this.queueSettings = {};
+
+            this.queueSettings.queue_cancelled_flag = false;
+            this.queueSettings.queue_upload_count = 0;
+
+            this.queueSettings.user_upload_complete_handler = this.settings.upload_complete_handler;
+            this.queueSettings.user_upload_start_handler = this.settings.upload_start_handler;
+            this.settings.upload_complete_handler = SWFUpload.queue.uploadCompleteHandler;
+            this.settings.upload_start_handler = SWFUpload.queue.uploadStartHandler;
+
+            this.settings.queue_complete_handler = this.settings.queue_complete_handler || null;
+        };
+    })(SWFUpload.prototype.initSettings);
+
+    SWFUpload.prototype.startUpload = function (fileID) {
+        this.queueSettings.queue_cancelled_flag = false;
+        this.callFlash("StartUpload", [fileID]);
+    };
+
+    SWFUpload.prototype.cancelQueue = function () {
+        this.queueSettings.queue_cancelled_flag = true;
+        this.stopUpload();
+
+        var stats = this.getStats();
+        while (stats.files_queued > 0) {
+            this.cancelUpload();
+            stats = this.getStats();
+        }
+    };
+
+    SWFUpload.queue.uploadStartHandler = function (file) {
+        var returnValue;
+        if (typeof(this.queueSettings.user_upload_start_handler) === "function") {
+            returnValue = this.queueSettings.user_upload_start_handler.call(this, file);
+        }
+
+        // To prevent upload a real "FALSE" value must be returned, otherwise default to a real "TRUE" value.
+        returnValue = (returnValue === false) ? false : true;
+
+        this.queueSettings.queue_cancelled_flag = !returnValue;
+
+        return returnValue;
+    };
+
+    SWFUpload.queue.uploadCompleteHandler = function (file) {
+        var user_upload_complete_handler = this.queueSettings.user_upload_complete_handler;
+        var continueUpload;
+
+        if (file.filestatus === SWFUpload.FILE_STATUS.COMPLETE) {
+            this.queueSettings.queue_upload_count++;
+        }
+
+        if (typeof(user_upload_complete_handler) === "function") {
+            continueUpload = (user_upload_complete_handler.call(this, file) === false) ? false : true;
+        } else if (file.filestatus === SWFUpload.FILE_STATUS.QUEUED) {
+            // If the file was stopped and re-queued don't restart the upload
+            continueUpload = false;
+        } else {
+            continueUpload = true;
+        }
+
+        if (continueUpload) {
+            var stats = this.getStats();
+            if (stats.files_queued > 0 && this.queueSettings.queue_cancelled_flag === false) {
+                this.startUpload();
+            } else if (this.queueSettings.queue_cancelled_flag === false) {
+                this.queueEvent("queue_complete_handler", [this.queueSettings.queue_upload_count]);
+                this.queueSettings.queue_upload_count = 0;
+            } else {
+                this.queueSettings.queue_cancelled_flag = false;
+                this.queueSettings.queue_upload_count = 0;
+            }
+        }
+    };
+}
+
+/*
+ A simple class for displaying file information and progress
+ Note: This is a demonstration only and not part of SWFUpload.
+ Note: Some have had problems adapting this class in IE7. It may not be suitable for your application.
+ */
+
+// Constructor
+// file is a SWFUpload file object
+// targetID is the HTML element id attribute that the FileProgress HTML structure will be added to.
+// Instantiating a new FileProgress object with an existing file will reuse/update the existing DOM elements
+function FileProgress(file, targetID) {
+    this.fileProgressID = file.id;
+
+    this.opacity = 100;
+    this.height = 0;
+
+
+    this.fileProgressWrapper = document.getElementById(this.fileProgressID);
+    if (!this.fileProgressWrapper) {
+        this.fileProgressWrapper = document.createElement("div");
+        this.fileProgressWrapper.className = "progressWrapper";
+        this.fileProgressWrapper.id = this.fileProgressID;
+
+        this.fileProgressElement = document.createElement("div");
+        this.fileProgressElement.className = "progressContainer";
+
+        var progressCancel = document.createElement("a");
+        progressCancel.className = "progressCancel";
+        progressCancel.href = "#";
+        progressCancel.style.visibility = "hidden";
+        progressCancel.appendChild(document.createTextNode(" "));
+
+        var progressText = document.createElement("div");
+        progressText.className = "progressName";
+        progressText.appendChild(document.createTextNode(file.name));
+
+        var progressBar = document.createElement("div");
+        progressBar.className = "progressBarInProgress";
+
+        var progressStatus = document.createElement("div");
+        progressStatus.className = "progressBarStatus";
+        progressStatus.innerHTML = "&nbsp;";
+
+        this.fileProgressElement.appendChild(progressCancel);
+        this.fileProgressElement.appendChild(progressText);
+        this.fileProgressElement.appendChild(progressStatus);
+        this.fileProgressElement.appendChild(progressBar);
+
+        this.fileProgressWrapper.appendChild(this.fileProgressElement);
+
+        document.getElementById(targetID).appendChild(this.fileProgressWrapper);
+    } else {
+        this.fileProgressElement = this.fileProgressWrapper.firstChild;
+        this.reset();
+    }
+
+    this.height = this.fileProgressWrapper.offsetHeight;
+    this.setTimer(null);
+
+
+}
+
+FileProgress.prototype.setTimer = function (timer) {
+    this.fileProgressElement["FP_TIMER"] = timer;
+};
+FileProgress.prototype.getTimer = function (timer) {
+    return this.fileProgressElement["FP_TIMER"] || null;
+};
+
+FileProgress.prototype.reset = function () {
+    this.fileProgressElement.className = "progressContainer";
+
+    this.fileProgressElement.childNodes[2].innerHTML = "&nbsp;";
+    this.fileProgressElement.childNodes[2].className = "progressBarStatus";
+
+    this.fileProgressElement.childNodes[3].className = "progressBarInProgress";
+    this.fileProgressElement.childNodes[3].style.width = "0%";
+
+    this.appear();
+};
+
+FileProgress.prototype.setProgress = function (percentage) {
+    this.fileProgressElement.className = "progressContainer green";
+    this.fileProgressElement.childNodes[3].className = "progressBarInProgress";
+    this.fileProgressElement.childNodes[3].style.width = percentage + "%";
+
+    this.appear();
+};
+FileProgress.prototype.setComplete = function () {
+    this.fileProgressElement.className = "progressContainer blue";
+    this.fileProgressElement.childNodes[3].className = "progressBarComplete";
+    this.fileProgressElement.childNodes[3].style.width = "";
+
+    var oSelf = this;
+    this.setTimer(setTimeout(function () {
+        oSelf.disappear();
+    }, 10000));
+};
+FileProgress.prototype.setError = function () {
+    this.fileProgressElement.className = "progressContainer red";
+    this.fileProgressElement.childNodes[3].className = "progressBarError";
+    this.fileProgressElement.childNodes[3].style.width = "";
+
+    var oSelf = this;
+    this.setTimer(setTimeout(function () {
+        oSelf.disappear();
+    }, 5000));
+};
+FileProgress.prototype.setCancelled = function () {
+    this.fileProgressElement.className = "progressContainer";
+    this.fileProgressElement.childNodes[3].className = "progressBarError";
+    this.fileProgressElement.childNodes[3].style.width = "";
+
+    var oSelf = this;
+    this.setTimer(setTimeout(function () {
+        oSelf.disappear();
+    }, 2000));
+};
+FileProgress.prototype.setStatus = function (status) {
+    this.fileProgressElement.childNodes[2].innerHTML = status;
+};
+
+// Show/Hide the cancel button
+FileProgress.prototype.toggleCancel = function (show, swfUploadInstance) {
+    this.fileProgressElement.childNodes[0].style.visibility = show ? "visible" : "hidden";
+    if (swfUploadInstance) {
+        var fileID = this.fileProgressID;
+        this.fileProgressElement.childNodes[0].onclick = function () {
+            swfUploadInstance.cancelUpload(fileID);
+            return false;
+        };
+    }
+};
+
+FileProgress.prototype.appear = function () {
+    if (this.getTimer() !== null) {
+        clearTimeout(this.getTimer());
+        this.setTimer(null);
+    }
+
+    if (this.fileProgressWrapper.filters) {
+        try {
+            this.fileProgressWrapper.filters.item("DXImageTransform.Microsoft.Alpha").opacity = 100;
+        } catch (e) {
+            // If it is not set initially, the browser will throw an error.  This will set it if it is not set yet.
+            this.fileProgressWrapper.style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity=100)";
+        }
+    } else {
+        this.fileProgressWrapper.style.opacity = 1;
+    }
+
+    this.fileProgressWrapper.style.height = "";
+
+    this.height = this.fileProgressWrapper.offsetHeight;
+    this.opacity = 100;
+    this.fileProgressWrapper.style.display = "";
+
+};
+
+// Fades out and clips away the FileProgress box.
+FileProgress.prototype.disappear = function () {
+
+    var reduceOpacityBy = 15;
+    var reduceHeightBy = 4;
+    var rate = 30;	// 15 fps
+
+    if (this.opacity > 0) {
+        this.opacity -= reduceOpacityBy;
+        if (this.opacity < 0) {
+            this.opacity = 0;
+        }
+
+        if (this.fileProgressWrapper.filters) {
+            try {
+                this.fileProgressWrapper.filters.item("DXImageTransform.Microsoft.Alpha").opacity = this.opacity;
+            } catch (e) {
+                // If it is not set initially, the browser will throw an error.  This will set it if it is not set yet.
+                this.fileProgressWrapper.style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity=" + this.opacity + ")";
+            }
+        } else {
+            this.fileProgressWrapper.style.opacity = this.opacity / 100;
+        }
+    }
+
+    if (this.height > 0) {
+        this.height -= reduceHeightBy;
+        if (this.height < 0) {
+            this.height = 0;
+        }
+
+        this.fileProgressWrapper.style.height = this.height + "px";
+    }
+
+    if (this.height > 0 || this.opacity > 0) {
+        var oSelf = this;
+        this.setTimer(setTimeout(function () {
+            oSelf.disappear();
+        }, rate));
+    } else {
+        this.fileProgressWrapper.style.display = "none";
+        this.setTimer(null);
+    }
 };
 
 /* Demo Note:  This demo uses a FileProgress class that handles the UI for displaying the file name and percent complete.
