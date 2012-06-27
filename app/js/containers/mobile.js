6412c495f77d5ff1c464e2c9058162010efcc0b0
RodEsp <rod_esp@hotmail.com>
2012-06-27 14:02:00 -0400

    Version 0.3.2. All (known) bugs fixed except for login screen back navigation.
    Code organized and commented.

--- app/js/containers/mobile.js
+++ app/js/containers/mobile.js
@@ -20,9 +20,6 @@ require.config({
         underscore: "../lib/underscore"
     },
     shim: {
-        cordova: {
-            exports: "c"
-        },
         underscore: {
             exports: "_"
         }
@@ -30,7 +27,20 @@ require.config({
 });
 
 require(["jquery", "lib/kendo.mobile.min", "developer", "db/services", "db/models"], function ($, k, developer, services, models) {
-    var mobile = {};
+    /**
+     * mobile = wrapper all mobile objects
+     * app = the kendoUI mobile app
+     * serviceDate = the date for the routes that are acquired form the server
+     * intervalId = used to start and stop a route
+     * trackPointsToSend = stores the track points that will be sent to the API
+     * e = the email used to login
+     * p = the password used to login
+     */
+    var mobile = {}, app, serviceDate, intervalId = null, trackPointsToSend = [], e, p;
+
+    //set mobile to a global function, so the functions are accessible from the HTML element
+    window.mobile = mobile;
+
     /**
      * The configuration object for the mobile application.
      * @const
@@ -52,10 +62,20 @@ require(["jquery", "lib/kendo.mobile.min", "developer", "db/services", "db/model
         ACCURACY_THRESHOLD: 50
     };
 
-    // Wait for Cordova to load
-    document.addEventListener("deviceready", onDeviceReady, false);
+    //Overrides phone's back button navigation - Phonegap
+    function onBack() {
+        if (window.location.hash === "#views/routeList.html") {
+            mobile.logout();
+        } else if (window.location.hash === "") {
+            app.navigate("#:back");
+        } else if (window.location.hash === "#views/routeDetails.html") {
+            app.navigate("views/routeList.html");
+        } else if (window.location.hash === "#views/routeDestinationDetails.html") {
+            app.navigate("views/routeDetails.html");
+        }
+    }
 
-    // Cordova is ready
+    //Fires when cordova is ready
     function onDeviceReady() {
         //set the OS of the device running the app
         mobile.CONFIG.DEVICE_PLATFORM = device.platform;
@@ -64,21 +84,55 @@ require(["jquery", "lib/kendo.mobile.min", "developer", "db/services", "db/model
         document.addEventListener("backbutton", onBack, false);
     }
 
-    function onBack () {
-        if(window.location.hash === "#views/routeList.html"){
-            navigator.notification.confirm("Going back will log you out, do you want to proceed?", function (buttonIndex) {
-                if(buttonIndex === 1) {
-                    mobile.logout();
-                };
-            }, "Warning", "Confirm,Cancel");
+    //Listens for Cordova to load
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    //region Login/Logout - Eventually will be moved to new navigator
+    mobile.login = function () {
+        if (localStorage.getItem("loggedIn") === "true") {
+            e = localStorage.getItem("email");
+            p = localStorage.getItem("pass");
         } else {
-            app.navigate("#:back");
+            e = $("#email").val();
+            p = $("#pass").val();
         }
+        services.authenticate(e, p, function (data) {
+            //if this was authenticated refresh routes and navigate to routeslist
+            if (data) {
+                //Save the application's login state.
+                localStorage.setItem("loggedIn", true);
+                localStorage.setItem("email", e);
+                localStorage.setItem("pass", p);
+                app.navigate("views/routeList.html");
+            } else {
+                navigator.notification.alert("Login information is incorrect.");
+            }
+        });
     };
 
-    var app;
-    var serviceDate, intervalId = null;
-    var trackPointsToSend = [];
+    mobile.logout = function () {
+        navigator.notification.confirm("Are you sure you want to logout?", function (buttonIndex) {
+            if (buttonIndex === 1) {
+                services.logout(function (data) {
+                    if (data) {
+                        //Clear application's login state.
+                        localStorage.setItem("loggedIn", false);
+                        mobile.viewModel.routesSource.data();
+                        app.navigate("views/clearhist.html");
+                    } else {
+                        navigator.notification.alert("Logout cannot be completed at this time.");
+                    }
+                });
+            }
+        }, "Logout", "Yes,No");
+    };
+
+    mobile.checkLogin = function () {
+        if (localStorage.getItem("loggedIn") === "true") {
+            mobile.login();
+        }
+    };
+    //endregion
 
     /**
      * Gets the latest trackpoint when called.
@@ -88,7 +142,6 @@ require(["jquery", "lib/kendo.mobile.min", "developer", "db/services", "db/model
      */
     var addPushTrackPoints = function (routeId) {
         var onSuccess = function (position) {
-            console.log("Position: " + position.coords.latitude + " " + position.coords.longitude);
             var collectedTime = new Date(position.timestamp);
 
             var newTrackPoint = new models.TrackPoint(
@@ -113,16 +166,15 @@ require(["jquery", "lib/kendo.mobile.min", "developer", "db/services", "db/model
         };
 
         var onError = function (error) {
-            console.log("Error Code: " + error.code + '\n' + error.message);
+            navigator.notification.alert("Can not collect track points at this time. Please check your GPS settings.", mobile.viewModel.endRoute(), "Alert", "Confirm");
+            //console.log("Error Code: " + error.code + '\n' + error.message);
         };
 
-        //native geolocation function
+        //Phonegap geolocation function
         navigator.geolocation.getCurrentPosition(onSuccess, onError, {enableHighAccuracy: true});
     };
 
-    /**
-     * Contains all the information and resources relating to the route view templates.
-     */
+    //region viewModel - Contains all the information and resources relating to the route view templates.
     mobile.viewModel = kendo.observable({
         routesSource: services.routesDataSource,
         /**
@@ -180,7 +232,6 @@ require(["jquery", "lib/kendo.mobile.min", "developer", "db/services", "db/model
         endVisible: false,
         /**
          * Starts collecting and sending trackpoints for the selected route.
-         * @param routeId
          */
         startRoute: function () {
             //the viewmodel
@@ -202,60 +253,14 @@ require(["jquery", "lib/kendo.mobile.min", "developer", "db/services", "db/model
             this.set("startVisible", true);
             this.set("endVisible", false);
 
-            var date = new Date();
-
             //stop calling addPushTrackPoints
             clearInterval(intervalId);
             trackPointsToSend = [];
         }
     });
+    //endregion
 
-    var e, p;
-//eventually will be moved to new navigator
-    mobile.login = function () {
-        if (localStorage.getItem("loggedIn") === "true") {
-            e = localStorage.getItem("email");
-            p = localStorage.getItem("pass");
-        } else {
-            e = $("#email").val();
-            p = $("#pass").val();
-        }
-        services.authenticate(e, p, function (data) {
-            //if this was authenticated refresh routes and navigate to routeslist
-            if (data) {
-                //Save the application's login state.
-                localStorage.setItem("loggedIn", true); localStorage["email"] = e; localStorage["pass"] = p;
-                app.navigate("views/routeList.html");
-            } else {
-                navigator.notification.alert("Login information is incorrect.");
-            }
-        });
-    };
-
-    mobile.logout = function () {
-        services.logout (function (data) {
-            if (data) {
-                //Clear application's login state.
-                localStorage.setItem("loggedIn", false);
-                mobile.viewModel.routesSource.data();
-                app.navigate("views/clearhist.html");
-            } else {
-                navigator.notification.alert("Logout cannot be completed at this time.");
-            }
-        });
-    };
-
-    //Must change to better design. Change if statement within mobile.login too.
-    mobile.checkLogin = function () {
-        if(localStorage.getItem("loggedIn") === "true") {
-            mobile.login();
-        }
-    };
-
-//set mobile to a global function, so the functions are accessible from the HTML element
-    window.mobile = mobile;
-
-    //Start the mobile application
-    app = new kendo.mobile.Application($(document.body), {platform: ""});
+    //Start the mobile application - must be at the bottom of the code.
+    app = new kendo.mobile.Application($(document.body));
 
 });
