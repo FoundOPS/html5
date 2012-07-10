fbd904102bca77541483c6091791d5678a3c1f52
Jonathan Perl <jperl@foundops.com>
2012-07-10 13:55:08 -0400

    Added google analytics, silverlight css/javascript. First attempt silverlight inside navigator

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,30 @@ require.config({
     }
 });
 
-require(["containers/navigator", "lib/kendo.all.min"], function (Navigator) {
+require(["containers/navigator", "lib/kendo.all.min", "view/silverlight"], function (Navigator) {
     //setup the navigator
     var n = new Navigator(window.initializeConfig);
     n.hideSearch();
 
-    //setup the application
-    new kendo.mobile.Application($("#content"), { initial: "view/testContent.html"});
+    //for debugging navigator
+    // var application = new kendo.mobile.Application($("#content"), { initial: "view/testContent.html"});
+
+    //for release
+
+    //setup page tracking
+    try {
+        var pageTracker = window._gat._getTracker("UA-25857232-1");
+        pageTracker._initData();
+        pageTracker._trackPageview();
+        function trackEvent(section, action, label) {
+            pageTracker._trackEvent(section, action, label);
+        }
+    }
+    catch (err) {
+    }
+
+    //initially load silverlight (so it starts loading)
+    var application = new kendo.mobile.Application($("#content"), { initial: "home/silverlight"});
+
+//    application.navigate("view/updates.html");
 });
---
+++ app/js/view/silverlight.js
@@ -0,0 +1,54 @@
+define(function () {
+    //Access function for silverlight to get around a security exception for Http Gets that do not have a crossdomainpolicy
+    window.httpGet = function (url) {
+        var img = new Image();
+        img.src = url;
+    };
+
+    //TODO MOVE TO NATIVE
+    window.openUserVoice = function () {
+        UserVoice.showPopupWidget();
+    };
+
+    window.onSilverlightError = function (sender, args) {
+        var appSource = "";
+        if (sender != null && sender != 0) {
+            appSource = sender.getHost().Source;
+        }
+
+        var errorType = args.ErrorType;
+        var iErrorCode = args.ErrorCode;
+
+        if (errorType == "ImageError" || errorType == "MediaError") {
+            return;
+        }
+
+        var errMsg = "Unhandled Error in Silverlight Application " + appSource + "\n";
+
+        errMsg += "Code: " + iErrorCode + "    \n";
+        errMsg += "Category: " + errorType + "       \n";
+        errMsg += "Message: " + args.ErrorMessage + "     \n";
+
+        if (errorType == "ParserError") {
+            errMsg += "File: " + args.xamlFile + "     \n";
+            errMsg += "Line: " + args.lineNumber + "     \n";
+            errMsg += "Position: " + args.charPosition + "     \n";
+        }
+        else if (errorType == "RuntimeError") {
+            if (args.lineNumber != 0) {
+                errMsg += "Line: " + args.lineNumber + "     \n";
+                errMsg += "Position: " + args.charPosition + "     \n";
+            }
+            errMsg += "MethodName: " + args.methodName + "     \n";
+        }
+
+        throw new Error(errMsg);
+    };
+
+    window.onSourceDownloadProgressChanged = function (sender, eventArgs) {
+        var myText = sender.findName("progressText");
+        myText.Text = (Math.round(eventArgs.progress * 100)).toString();
+        var myBar = sender.findName("ProgressBarTransform");
+        myBar.ScaleX = eventArgs.progress;
+    };
+});
