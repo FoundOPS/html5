21156134a9f7db8bad5558e512740b197e2dda79
Jonathan Perl <jperl@foundops.com>
2012-07-11 16:24:50 -0400

    moving around some main app code

--- app/js/main.js
+++ app/js/main.js
@@ -12,21 +12,25 @@ require.config({
 });
 
 require(["containers/navigator", "silverlight", "underscore", "lib/kendo.all.min"], function (Navigator, silverlight) {
-    //setup the navigator
-    var n = new Navigator(window.navigatorConfig);
-    n.hideSearch();
-
     //setup page tracking
     try {
         var pageTracker = window._gat._getTracker("UA-25857232-1");
         pageTracker._initData();
         pageTracker._trackPageview();
-        function trackEvent(section, action, label) {
+        window.trackEvent = function (section, action, label) {
             pageTracker._trackEvent(section, action, label);
-        }
+        };
     }
     catch (err) {
     }
+
+    //setup the navigator
+    var n = new Navigator(window.navigatorConfig);
+    n.hideSearch();
+
+    $(document).on('click', ".sideBarElement", function(){
+        console.log("sideBarElement event fired!: "+$(this));
+    });
 
     var application = new kendo.mobile.Application($("#remoteContent"), { initial: "view/updates.html"});
     silverlight.show();
