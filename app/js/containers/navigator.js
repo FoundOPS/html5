cce4287e654fe6a46fd11685750395cca525097b
jkelly <jkelly@foundops.com>
2012-07-12 16:42:16 -0400

    Made change that should've been in last commit.

--- app/js/containers/navigator.js
+++ app/js/containers/navigator.js
@@ -536,7 +536,7 @@ define(["jquery", "lib/jquery.mousewheel", "lib/jquery.jScrollPane", "lib/kendo.
         }
         $("#sideBarSections").html(sBarElement);
 
-        $(document).on({
+        $(".sideBarElement").on({
             "touchstart mouseenter": function () {
                 $(this).stop(true, true).addClass($(this).attr('data-color'));
                 //var image = $(this).find(".icon:first").css('background-image').replace(/^url|[\(\)]/g, '');
@@ -554,7 +554,7 @@ define(["jquery", "lib/jquery.mousewheel", "lib/jquery.jScrollPane", "lib/kendo.
                 var section = getSection(config.sections, name);
                 $(this).trigger("sectionSelected", section);
             }
-        }, ".sideBarElement");
+        });
     };
 
     /** Initializes sidebar navigation **/
