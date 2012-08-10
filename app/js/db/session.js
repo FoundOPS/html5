be3ae17c0da848d575195e09ca3e8b08cb1b9ab1
RodEsp <rod_esp@hotmail.com>
2012-08-10 15:21:55 -0400

    Login webpage for mobile.

--- app/js/db/session.js
+++ app/js/db/session.js
@@ -58,132 +58,132 @@ define(['db/services', "lib/kendo.all"], function (dbservices) {
 
 //    for debugging when the API is turned off
 //    set static values set for config & selected role
-//    session._data = {
-//        "name": "Jonathan Perl",
-//        "settingsUrl": "#view/personalSettings.html",
-//        "logOutUrl": "../Account/LogOut",
-//        "avatarUrl": "img/emptyPerson.png",
-//        "roles": [
-//            {
-//                "id": "50f2551f-d3fd-4372-b5ed-b85f2cda95bb",
-//                "name": "AB Couriers",
-//                "type": "Administrator",
-//                "sections": [
-//                    "Clients",
-//                    "Dispatcher",
-//                    "Employees",
-//                    "Support",
-//                    "Locations",
-//                    "Regions",
-//                    "Services",
-//                    "Vehicles"
-//                ]
-//            },
-//            {
-//                "id": "8db0dfbc-210c-4117-a010-511cb1afbcff",
-//                "name": "FoundOPS",
-//                "sections": [
-//                    "Business Accounts"
-//                ]
-//            },
-//            {
-//                "id": "b042f3e3-99e4-463f-92cd-aa39f1667279",
-//                "name": "Generic Oil Collector",
-//                "type": "Administrator",
-//                "sections": [
-//                    "Clients",
-//                    "Dispatcher",
-//                    "Employees",
-//                    "Support",
-//                    "Locations",
-//                    "Regions",
-//                    "Services",
-//                    "Vehicles"
-//                ]
-//            },
-//            {
-//                "id": "9fda11d1-428f-4f60-b6e1-9e31528c75b7",
-//                "name": "GotGrease?",
-//                "type": "Administrator",
-//                "businessLogoUrl": "./img/got-grease-logo.png",
-//                "sections": [
-//                    "Clients",
-//                    "Dispatcher",
-//                    "Employees",
-//                    "Support",
-//                    "Locations",
-//                    "Regions",
-//                    "Services",
-//                    "Vehicles"
-//                ]
-//            }
-//        ],
-//        "sections": [
-//            {
-//                "name": "Business Accounts",
-//                "color": "black",
-//                "iconUrl": "img/businessAccounts.png",
-//                "hoverIconUrl": "img/businessAccountsColor.png",
-//                "isSilverlight": true
-//            },
-//            {
-//                "name": "Clients",
-//                "color": "blue",
-//                "iconUrl": "img/clients.png",
-//                "hoverIconUrl": "img/clientsColor.png",
-//                "isSilverlight": true
-//            },
-//            {
-//                "name": "Dispatcher",
-//                "color": "green",
-//                "iconUrl": "img/dispatcher.png",
-//                "hoverIconUrl": "img/dispatcherColor.png",
-//                "isSilverlight": true
-//            },
-//            {
-//                "name": "Employees",
-//                "color": "red",
-//                "iconUrl": "img/employees.png",
-//                "hoverIconUrl": "img/employeesColor.png",
-//                "isSilverlight": true
-//            },
-//            {
-//                "name": "Support",
-//                "color": "blue",
-//                "iconUrl": "img/uservoice.png",
-//                "hoverIconUrl": "img/uservoiceColor.png"
-//            },
-//            {
-//                "name": "Locations",
-//                "color": "orange",
-//                "iconUrl": "img/locations.png",
-//                "hoverIconUrl": "img/locationsColor.png",
-//                "isSilverlight": true
-//            },
-//            {
-//                "name": "Regions",
-//                "color": "orange",
-//                "iconUrl": "img/regions.png",
-//                "hoverIconUrl": "img/regionsColor.png",
-//                "isSilverlight": true
-//            },
-//            {
-//                "name": "Services",
-//                "color": "green",
-//                "iconUrl": "img/services.png",
-//                "hoverIconUrl": "img/servicesColor.png",
-//                "isSilverlight": true
-//            },
-//            {
-//                "name": "Vehicles",
-//                "color": "red",
-//                "iconUrl": "img/vehicles.png",
-//                "hoverIconUrl": "img/vehiclesColor.png",
-//                "isSilverlight": true
-//            }
-//        ]
-//    };
-//    session._role = session._data.roles[0];
+    session._data = {
+        "name": "Jonathan Perl",
+        "settingsUrl": "#view/personalSettings.html",
+        "logOutUrl": "../Account/LogOut",
+        "avatarUrl": "img/emptyPerson.png",
+        "roles": [
+            {
+                "id": "50f2551f-d3fd-4372-b5ed-b85f2cda95bb",
+                "name": "AB Couriers",
+                "type": "Administrator",
+                "sections": [
+                    "Clients",
+                    "Dispatcher",
+                    "Employees",
+                    "Support",
+                    "Locations",
+                    "Regions",
+                    "Services",
+                    "Vehicles"
+                ]
+            },
+            {
+                "id": "8db0dfbc-210c-4117-a010-511cb1afbcff",
+                "name": "FoundOPS",
+                "sections": [
+                    "Business Accounts"
+                ]
+            },
+            {
+                "id": "b042f3e3-99e4-463f-92cd-aa39f1667279",
+                "name": "Generic Oil Collector",
+                "type": "Administrator",
+                "sections": [
+                    "Clients",
+                    "Dispatcher",
+                    "Employees",
+                    "Support",
+                    "Locations",
+                    "Regions",
+                    "Services",
+                    "Vehicles"
+                ]
+            },
+            {
+                "id": "9fda11d1-428f-4f60-b6e1-9e31528c75b7",
+                "name": "GotGrease?",
+                "type": "Administrator",
+                "businessLogoUrl": "./img/got-grease-logo.png",
+                "sections": [
+                    "Clients",
+                    "Dispatcher",
+                    "Employees",
+                    "Support",
+                    "Locations",
+                    "Regions",
+                    "Services",
+                    "Vehicles"
+                ]
+            }
+        ],
+        "sections": [
+            {
+                "name": "Business Accounts",
+                "color": "black",
+                "iconUrl": "img/businessAccounts.png",
+                "hoverIconUrl": "img/businessAccountsColor.png",
+                "isSilverlight": true
+            },
+            {
+                "name": "Clients",
+                "color": "blue",
+                "iconUrl": "img/clients.png",
+                "hoverIconUrl": "img/clientsColor.png",
+                "isSilverlight": true
+            },
+            {
+                "name": "Dispatcher",
+                "color": "green",
+                "iconUrl": "img/dispatcher.png",
+                "hoverIconUrl": "img/dispatcherColor.png",
+                "isSilverlight": true
+            },
+            {
+                "name": "Employees",
+                "color": "red",
+                "iconUrl": "img/employees.png",
+                "hoverIconUrl": "img/employeesColor.png",
+                "isSilverlight": true
+            },
+            {
+                "name": "Support",
+                "color": "blue",
+                "iconUrl": "img/uservoice.png",
+                "hoverIconUrl": "img/uservoiceColor.png"
+            },
+            {
+                "name": "Locations",
+                "color": "orange",
+                "iconUrl": "img/locations.png",
+                "hoverIconUrl": "img/locationsColor.png",
+                "isSilverlight": true
+            },
+            {
+                "name": "Regions",
+                "color": "orange",
+                "iconUrl": "img/regions.png",
+                "hoverIconUrl": "img/regionsColor.png",
+                "isSilverlight": true
+            },
+            {
+                "name": "Services",
+                "color": "green",
+                "iconUrl": "img/services.png",
+                "hoverIconUrl": "img/servicesColor.png",
+                "isSilverlight": true
+            },
+            {
+                "name": "Vehicles",
+                "color": "red",
+                "iconUrl": "img/vehicles.png",
+                "hoverIconUrl": "img/vehiclesColor.png",
+                "isSilverlight": true
+            }
+        ]
+    };
+    session._role = session._data.roles[0];
 
     return session;
 });
