0b4eeb315a0cde86cb2a3669b6704556e63f26a5
RodEsp <rod_esp@hotmail.com>
2012-08-15 19:10:53 -0400

    Automatic app building - 99%.

--- app-built/js/db/developer.js
+++
@@ -1,3 +0,0 @@
-// Copyright 2012 FoundOPS LLC. All Rights Reserved.
-
-define([],function(){var developer={};return developer.DataSource={BROWSER_LOCALAPI:0,ANDROID_LOCALAPI:1,LIVE:2,TESTAPI:3},developer.Frame={SILVERLIGHT:0,BROWSER:1},developer.CURRENT_DATA_SOURCE=developer.DataSource.BROWSER_LOCALAPI,developer.CURRENT_FRAME=developer.Frame.BROWSER,developer.GOTGREASE_ROLE_ID="7E31F602-C6AA-4F7A-AF46-E8FC1642DAB2",developer})
--- app/js/db/developer.js
+++ app/js/db/developer.js
@@ -38,7 +38,7 @@ define(function () {
      * The current web service source. Used when running local server for debugging.
      * @type {developer.DataSource}
      */
-    developer.CURRENT_DATA_SOURCE = developer.DataSource.BROWSER_LOCALAPI;
+    developer.CURRENT_DATA_SOURCE = developer.DataSource.LIVE;
 
     /**
      * This is for sections that are in the Silverlight application and is used for debugging.
