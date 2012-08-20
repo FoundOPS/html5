3cae11c9adc520cebb8590572ec14ddcea8e7d6e
RodEsp <rod_esp@hotmail.com>
2012-08-20 19:19:18 -0400

    Built app working on phone/unbuilt app is not. (Must fix navigator-sideBar links)

--- Android - Built/src/com/foundops/app/FoundOPS.java
+++ Android - Built/src/com/foundops/app/FoundOPS.java
@@ -10,7 +10,7 @@ public class FoundOPS extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/navigator.html");
     }
 
     @Override
