7348c1d63d27a5863c70ab2a22c2d5fcb6724ad2
RodEsp <rod_esp@hotmail.com>
2012-08-28 14:33:18 -0400

    Added back button to navigator when running in mobile device.

--- app/js/main.js
+++ app/js/main.js
@@ -118,9 +118,10 @@ require(["widgets/navigator", "containers/silverlight", "db/session", "db/models
 
     //Overrides phone's back button navigation - Phonegap
     main.onBack = function () {
+        $('#backButtonContainer').toggleClass("clicked");
         if (window.location.hash === "#view/updates.html") {
-            alert("You will now be logged out.");
-            application.navigate("view/logout.html");
+            var r = confirm("Are you sure you would like to log out?");
+            if (r) { application.navigate("view/logout.html"); }
         } else if (window.location.hash === "#view/routes.html") {
             application.navigate("view/updates.html");
         } else if (window.location.hash === "#view/routeDetails.html") {
@@ -130,6 +131,9 @@ require(["widgets/navigator", "containers/silverlight", "db/session", "db/models
         } else if (window.location.hash === "#view/routeTask.html") {
             application.navigate("view/routeDestinationDetails.html");
         }
+        setTimeout (function () {
+            $('#backButtonContainer').toggleClass("clicked");
+        }, 100);
     };
 
     // Fires when Cordova is ready
