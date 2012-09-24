0af5282a09448d9d47bd1539ddd18379a76a3b0e
RodEsp <rod_esp@hotmail.com>
2012-09-24 15:22:19 -0400

    Add android eclipse project

---
+++ build/mobile/Android/src/foundOps/mobile/FoundOPS.java
@@ -0,0 +1,20 @@
+package foundOps.mobile;
+
+import org.apache.cordova.*;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class FoundOPS extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_found_ops, menu);
+		return true;
+	}
+}
