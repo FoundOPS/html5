250323d0e295fc06de3b6e4454e0d4bbf7ed470d
apohl1111 <apohl@foundops.com>
2012-09-28 16:29:43 -0400

    begin adding analytics

---
+++ app/js/tools/analytics.js
@@ -0,0 +1,15 @@
+// Copyright 2012 FoundOPS LLC. All Rights Reserved.
+
+/**
+ * @fileoverview Class to hold analytics.
+ */
+
+"use strict";
+
+define(['jquery'], function ($) {
+    var analytics = {};
+
+
+
+    return analytics;
+});
