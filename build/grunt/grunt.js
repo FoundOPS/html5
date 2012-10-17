294b110a50cc3291118b5ac8282036d782a70fd6
RodEsp <rod_esp@hotmail.com>
2012-10-17 12:51:12 -0400

    npm install works now with package.json
    It installs all necessary dependencies and Build.md has been modified to reflect those changes.

--- build/grunt/grunt.js
+++ build/grunt/grunt.js
@@ -76,59 +76,61 @@ module.exports = function (grunt) {
             }
         },
         requirejs: {
-            almond: true,
-            baseUrl: "../../app/js",
-            paths: {
-                //root paths
-                lib: "../lib",
-                //libraries
-                colorpicker: "ui/colorPicker",
-                doT: "../lib/doT.min",
-                hasher: "../lib/hasher",
-                kendo: "../lib/kendo.all",
-                jautosize: "../lib/jquery.autosize",
-                jfilereader: "../lib/jquery.FileReader",
-                jform: "../lib/jquery.form",
-                jmaskmoney: "../lib/jquery.maskMoney",
-                jmousewheel: "../lib/jquery.mousewheel",
-                jquery: "../lib/jquery",
-                jscrollpane: "../lib/jquery.jScrollPane",
-                jtooltip: "../lib/jquery.tooltip.min",
-                jui: "../lib/jquery-ui-1.8.21.core.min",
-                moment: "../lib/moment",
-                noty: "../lib/noty",
-                select2: "../lib/select2",
-                signals: "../lib/signals",
-                totango: "../lib/totango",
-                underscore: "../lib/underscore",
-                "underscore.string": "../lib/underscore.string",
-                uservoice: "../lib/userVoice"
-            },
-            shim: {
-                underscore: {
-                    exports: '_'
+            std: {
+                almond: true,
+                baseUrl: "../../app/js",
+                paths: {
+                    //root paths
+                    lib: "../lib",
+                    //libraries
+                    colorpicker: "ui/colorPicker",
+                    doT: "../lib/doT.min",
+                    hasher: "../lib/hasher",
+                    kendo: "../lib/kendo.all",
+                    jautosize: "../lib/jquery.autosize",
+                    jfilereader: "../lib/jquery.FileReader",
+                    jform: "../lib/jquery.form",
+                    jmaskmoney: "../lib/jquery.maskMoney",
+                    jmousewheel: "../lib/jquery.mousewheel",
+                    jquery: "../lib/jquery",
+                    jscrollpane: "../lib/jquery.jScrollPane",
+                    jtooltip: "../lib/jquery.tooltip.min",
+                    jui: "../lib/jquery-ui-1.8.21.core.min",
+                    moment: "../lib/moment",
+                    noty: "../lib/noty",
+                    select2: "../lib/select2",
+                    signals: "../lib/signals",
+                    totango: "../lib/totango",
+                    underscore: "../lib/underscore",
+                    "underscore.string": "../lib/underscore.string",
+                    uservoice: "../lib/userVoice"
                 },
-                colorpicker: ['jquery'],
-                hasher: ['signals'],
-                jautosize: ['jquery'],
-                jfilereader: ['jquery'],
-                jform: ['jquery'],
-                jmaskmoney: ['jquery'],
-                jmousewheel: ['jquery'],
-                jscrollpane: ['jquery'],
-                jtooltip: ['jquery'],
-                jui: ['jquery'],
-                kendo: ['jquery'],
-                moment: {},
-                noty: ['jquery'],
-                select2: ['jquery'],
-                uservoice: {},
-                totango: {}
-            },
-            include: ["main"],
-            //cannot wrap or else there will be bugs with external libraries (AKA UserVoice closing bug)
-            wrap: false,
-            out: "../main/main-built.js"
+                shim: {
+                    underscore: {
+                        exports: '_'
+                    },
+                    colorpicker: ['jquery'],
+                    hasher: ['signals'],
+                    jautosize: ['jquery'],
+                    jfilereader: ['jquery'],
+                    jform: ['jquery'],
+                    jmaskmoney: ['jquery'],
+                    jmousewheel: ['jquery'],
+                    jscrollpane: ['jquery'],
+                    jtooltip: ['jquery'],
+                    jui: ['jquery'],
+                    kendo: ['jquery'],
+                    moment: {},
+                    noty: ['jquery'],
+                    select2: ['jquery'],
+                    uservoice: {},
+                    totango: {}
+                },
+                include: ["main"],
+                //cannot wrap or else there will be bugs with external libraries (AKA UserVoice closing bug)
+                wrap: false,
+                out: "../main/main-built.js"
+            }
         },
         //delete the directories before recreating them
         clean: ["C:/FoundOPS/html5/build/main", "C:/FoundOPS/html5/build/mobile/Android/assets/www", "C:/FoundOPS/html5/build/mobile/iOS/www"],
