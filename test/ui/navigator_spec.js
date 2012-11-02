746256af5774a82da62480062913560cb9319e89
jperl <jperl@foundops.com>
2012-11-02 19:27:18 -0400

    now with jasmine-jquery. first test case! woooooooo!!!!!!!!!!!!!!!!!!!!!

--- test/ui/navigator_spec.js
+++ test/ui/navigator_spec.js
@@ -1,7 +1,15 @@
 jasmineui.loadUi("/app/navigator-test.html", function () {
     describe('navigator', function () {
-        it("should show the popup when the client icon is clicked", function () {
-            alert("KING IN THE CASTLE!");
+        it("should show the popup when the icon is clicked", function () {
+            runs(function () {
+                var button = document.getElementById('navClient');
+                jasmineui.simulate(button, 'click');
+            });
+            runs(function () {
+                setTimeout(function () {
+                    expect(document.getElementById("popupWrapper")).toBeVisible();
+                }, 1500);
+            });
         });
     });
 });
