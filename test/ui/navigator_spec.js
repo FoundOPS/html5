dcd7546f8a7f63bead2cd69fdc90b4c116b4b261
jperl <jperl@foundops.com>
2012-11-02 19:12:56 -0400

    Got it to work!

--- test/ui/navigator_spec.js
+++ test/ui/navigator_spec.js
@@ -1,17 +1,7 @@
 jasmineui.loadUi("/app/navigator-test.html", function () {
-//    jasmineui.inject(function () {
-//        //some functions infinitely use setInterval (i think debounce on underscore) (todo try and reduce this)
-//        //the async sensor will never have 0 intervals and will cause the test suite to timeout
-//        delete jasmineui.asyncSensors["interval"];
-//
-//        console.log("");
-//    });
-
     describe('navigator', function () {
-        alert("HERE");
         it("should show the popup when the client icon is clicked", function () {
-            console.log("Got here");
-            alert("PLEASE");
+            alert("KING IN THE CASTLE!");
         });
     });
 });
