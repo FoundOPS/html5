4c4b65e4168c9168d195bdd2300a6ce76acfb5a0
jperl <jperl@foundops.com>
2012-11-15 14:26:41 -0500

    Fixed signature field not updating. Will not move back until sig is saved. Need to do offline support and fix/remove interdependencies of sections

--- app/js/sections/signature.js
+++ app/js/sections/signature.js
@@ -1,46 +1,41 @@
 // Copyright 2012 FoundOPS LLC. All Rights Reserved.
 
 /**
- * @fileoverview Class to hold the selected route task signature's logic.
+ * @fileoverview Class to hold the selected route task signature's logic
  */
 
 'use strict';
 
-define(["sections/routeTask", "db/services", "db/saveHistory", "tools/parameters", "underscore", "tools/kendoTools", "jsignature", "jsigbase30", "jsigSVG"],
+define(["sections/routeTask", "db/services", "db/saveHistory", "tools/parameters", "underscore", "jsignature", "jsigbase30", "jsigSVG"],
     function (routeTask, dbServices, saveHistory, parameters, _) {
         /**
          * vm = viewModel
          * popupCaller = the button who's click opened the popup.
          */
-        var section = {}, vm = kendo.observable(), popupCaller;
-        window.signature = section;
-
+        var section = {}, vm = kendo.observable();
         section.vm = vm;
+
+        window.signature = section;
 
 //public methods
 
         section.initialize = function () {
             $(".sigPad").jSignature();
         };
+
         section.onBack = function () {
-            vm.closeSigPad();
+            closeSigPad();
             var query = parameters.get();
             delete query.signatureId;
             parameters.set({params: query, replace: true, section: {name: "routeTask"}});
         };
-        section.show = function () {
-            //TODO replace this with centralized access to loaded entities in new datamanager
-            var service = routeTask.vm.get("selectedService");
-//            parameters.get().signatureId;
 
-            vm.openSigPad();
+        section.show = function () {
+            openSigPad();
         };
 
 //signature pad methods
-        vm.resetSigPad = function () {
-            $(".sigPad").jSignature("reset");
-        };
-        vm.openSigPad = function () {
+        var openSigPad = function () {
             vm.resetSigPad();
             var width = $(window).width();
             var margin = width > 800 ? (width - 800) / 2 : 0;
@@ -52,7 +47,7 @@ define(["sections/routeTask", "db/services", "db/saveHistory", "tools/parameters
                 $('#nav, #sideBarWrapper').css('visibility', 'hidden');
             });
         };
-        vm.closeSigPad = function () {
+        var closeSigPad = function () {
             $(".sigWrapper").animate({"opacity": 0}, 300, function () {
                 $("#content").css("padding", "");
             });
@@ -61,29 +56,35 @@ define(["sections/routeTask", "db/services", "db/saveHistory", "tools/parameters
                 vm.resetSigPad();
             });
         };
+
+        vm.resetSigPad = function () {
+            $(".sigPad").jSignature("reset");
+        };
+
         vm.saveSig = function () {
-            if ($('.sigPad').jSignature("getData", "base30")[1] !== "") {
-                //TODO: Use field ID, not Type.
+            //check there is a signature
+            if ($('.sigPad').jSignature("getData", "base30")[1]) {
                 var sigField = _.find(routeTask.vm.get("selectedService.Fields"), function (field) {
                     return field.Id === parameters.get().signatureId;
                 });
                 sigField.set("Value", $('.sigPad').jSignature("getData", "base30")[1]);
-                setTimeout(function () {
+                routeTask.save(function () {
                     //allow the value to propagate
                     section.onBack();
-                }, 200);
+                });
             } else {
                 alert("Please sign before you save or press the cancel button to go back.");
             }
         };
 
-        //Handle css upon device rotation.
-        //Detect whether device supports orientationchange event, otherwise fall back to the resize event.
+        //Detect whether device supports orientation change event, otherwise fall back to the resize event
         var supportsOrientationChange = "onorientationchange" in window,
             orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+        //Reset css upon device rotation
         window.addEventListener(orientationEvent, function () {
-            if (parameters.get().signatureId) {
-                vm.openSigPad();
+            if (parameters.getSection().name === "signature") {
+                openSigPad();
             }
         }, false);
 
