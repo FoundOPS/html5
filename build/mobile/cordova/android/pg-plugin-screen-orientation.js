3fc14cff289a9142de8749c5021fa9b88e5db064
RodEsp <respinosa@foundops.com>
2012-11-19 12:54:10 -0500

    Got rid of screen orientation plugins for cordova.

--- build/mobile/cordova/android/pg-plugin-screen-orientation.js
+++
@@ -1,6 +0,0 @@
-var screenOrientation = function() {}
-
-screenOrientation.prototype.set = function(str, success, fail) {
-    PhoneGap.exec(success, fail, "ScreenOrientation", "set", [str]);
-};
-navigator.screenOrientation = new screenOrientation();
