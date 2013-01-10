bd05f8cba4c189c7a43171338e1f6220ced46a99
kellyje <kellyje@purdue.edu>
2013-01-10 16:06:32 -0500

    Finished most of Statuses.

--- app/js/vendor/statuses.js
+++ app/js/vendor/statuses.js
@@ -1,5 +1,53 @@
 "use strict";
 !function ($) {
+    var methods = {
+        init: function () {
+            console.log("Init.");
+            var loadStatus = function(){
+                console.log("Status loaded.");
+                var navDiv = $("#nav");
+                return new Statuses(navDiv, null, null);
+            };
+            if($("#nav").length===0){
+                $(document).on("navigator.loaded", function(){
+                    return loadStatus();
+                });
+            }else{
+                return loadStatus();
+            }
+        },
+        setState: function(status, state){
+            console.log("Setting state.");
+            status.setState(state);
+        },
+        setUndoMode: function(status, mode){
+            console.log("Setting mode.");
+            status.setUndoMode(mode);
+        }
+    };
+
+
+    $.fn.status = function (method) {
+        // Create some defaults, extending them with any options that were provided
+        //var settings = $.extend({}, options);
+        // Method calling logic
+
+        if (methods[method]) {
+            if(method!=='init'&&!navigator){
+                $.error('Navigator not initialized!');
+                return;
+            }
+            return methods[ method ].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.navigator');
+        }
+
+        return this.each(function () {});
+    };
+
+
     //////////////////////////
     //  Enums
     //////////////////////////
@@ -23,16 +71,38 @@
             "../img/errorStatus.png" //State 2
         ];
 
-    var INIT_STATUS_IMG = statusImages[states.error];
+    var INIT_STATUS_IMG = statusImages[states.idle];
+
+    var createStatusMenu = function(status, mode){
+        var popupClass = $(document).popup("getPopupClass");
+        var contents = [];
+        var menu = {
+            id: "navStatus",
+            contents: contents,
+            disableHeader: true
+        };
+
+        var undoLastMenu = {"name": "Undo Last", id: "undoLast"};
+        var undoAllMenu = {"name": "Undo All", id: "undoAll"};
+
+        if(status.currentUndoMode>=1){
+            contents.push(undoLastMenu);
+        }
+        if(status.currentUndoMode>=2){
+            contents.push(undoAllMenu);
+        }
+        return menu;
+    };
 
     //////////////////////////
     //  Constructor
     //////////////////////////
     function Statuses(targetDiv, undoLast, undoAll){
-       var thisStatus = this;
+        var thisStatus = this;
         this.targetDiv = targetDiv;
         this.currentState = states.idle;
         this.currentUndoMode = undoMode.all;
+        this.statusPopup = null;
 
         this.onUndoLast = function(){};
         this.onUndoAll = function(){};
@@ -47,25 +117,8 @@
             $(targetDiv).append(statusDiv);
 
             //Init Popup.
-            var contents = [];
-            var undoLastMenu = {"name": "Undo Last", id: "undoLast"};
-            var undoAllMenu = {"name": "Undo All", id: "undoAll"};
-            if(thisStatus.currentUndoMode>=1){
-                contents.push(undoLastMenu);
-            }
-            if(thisStatus.currentUndoMode>=2){
-                contents.push(undoAllMenu);
-            }
-
-            if(contents.lenght===0){
-                //TODO: Disable popup.
-            }
-
-            $("#navStatus").optionsPopup({
-                id: "navStatus",
-                contents: contents,
-                disableHeader: true
-            });
+            var menu = createStatusMenu(thisStatus, thisStatus.currentUndoMode);
+            thisStatus.statusPopup = $("#navStatus").optionsPopup(menu);
 
             $(document).on("click", "#undoLast", function(){
                 $(document).trigger("status.undoLast");
@@ -88,17 +141,42 @@
         this.currentState = state;
 
         $("#navStatus .navIcon").attr("src", statusImages[state]);
+
+        $(document).trigger("status.stateChange");
     };
 
     Statuses.prototype.setUndoMode = function(mode){
+        var previousMode = this.currentUndoMode;
         this.currentUndoMode = mode;
 
-        //TODO: Update undo menu.
-    };
+        var popupClass = this.statusPopup.superConstructor;
+        var menu = popupClass.getMenu("navStatus");
+        var newMenu = null;
 
-    //TODO: Move into navigator.
-    $(document).on("navigator.loaded", function(){
-        var navDiv = $("#nav");
-        var s = new Statuses(navDiv, null, null);
-    });
+        if(previousMode === mode){
+            return;
+        }
+
+        popupClass.closePopup();
+
+        if(this.currentUndoMode === undoMode.none){
+            this.statusPopup.disablePopup();
+        }else{
+            this.statusPopup.enablePopup();
+        }
+
+        newMenu = createStatusMenu(this, mode);
+        console.log("currentUndoMode: "+mode);
+
+        //Overwrite properties //TODO: Move into popup function, replaceMenu(oldMenu, newMenu).
+        var property;
+        for(property in menu){
+            delete menu[property];
+        }
+        for(property in newMenu){
+            menu[property] = newMenu[property];
+        }
+
+        $(document).trigger("status.undoModeChange");
+    };
 }(window.jQuery);
